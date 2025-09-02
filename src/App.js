bfa7b90d4e2c303475e901c59a2343376bfaf7dd
Adrian Toman <adrian@adriantoman.com>
2025-09-03 08:31:04 +1000

    Initial React scaffold

--- src/App.js
+++ src/App.js
@@ -3,21 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "#000",
+        color: "#fff",
+        fontSize: "1.5rem"
+      }}
+    >
+      Swipeable Video Feed Placeholder
     </div>
   );
 }
